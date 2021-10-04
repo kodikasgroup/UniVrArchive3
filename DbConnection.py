5e1272bced31d05c965ed78a6df8c38479fbabba
Lorenzo Bonanni <lorenzo.bonanni@studenti.univr.it>
2021-10-04 19:21:30 +0200

    added method __execute_query and get_credits

--- DbConnection.py
+++ DbConnection.py
@@ -6,6 +6,10 @@ class DbConnection:
     __instance = None
 
     def __initialize(self):
+        """
+        establish connection to database
+        :return:
+        """
         path = 'db/UserLogDB.db'
         try:
             self.conn = sqlite3.connect(path)
@@ -13,6 +17,23 @@ class DbConnection:
         except sqlite3.Error as error:
             logging.debug("An Error Occurred during db connection: ")
             logging.debug(f"{error}")
+
+    def __execute_query(self, query: str, chat_id: int):
+        """
+        executes the given query and then returns the result of the execution
+        :param query:
+        :param chat_id:
+        :return:
+        """
+        cur = self.conn.cursor()
+        cur.execute(query)
+        result = cur.fetchall()
+        if result is None:
+            logging.debug(f"ERROR invalid chat id: {chat_id}")
+            return 0
+        else:
+            if len(result) == 1:
+                return result[0][0]
 
     @staticmethod
     def get_instance():
@@ -27,12 +48,14 @@ class DbConnection:
         :param chat_id: the id of the user
         :return:
         """
-        cur = self.conn.cursor()
         query = f"SELECT Vip FROM Reserved WHERE chat_id=={chat_id}"
-        cur.execute(query)
-        result = cur.fetchone()
-        if result is None:
-            logging.debug(f"ERROR invalid chat id: {chat_id}")
-            return 0
-        else:
-            return result[0]
+        return self.__execute_query(query, chat_id)
+
+    def get_credits(self, chat_id: int) -> int:
+        """
+        retrieves the number of credits of the given chat id
+        :param chat_id: the id of the user
+        :return:
+        """
+        query = f"SELECT Credit FROM Reserved WHERE chat_id=={chat_id}"
+        return self.__execute_query(query, chat_id)
--- DbHandler.py
+++ DbHandler.py
@@ -18,5 +18,22 @@ class DbHandler:
         logging.debug(f"User with chat_id: {chat_id}, has a vip value: {value}")
         return value
 
+    @staticmethod
+    def get_credits(chat_id: int) -> int:
+        """
+        retrieves the number of credits of the given chat id
+        :param chat_id: the id of the user
+        :return:
+        """
+        value = DbHandler.db_connection.get_credits(chat_id)
+        logging.debug(f"User with chat_id: {chat_id}, has {value} credits")
+        return value
 
-print(DbHandler.is_vip(1234))
+
+
+    # TODO add method increase downloads
+    # TODO add method decrease credits
+
+
+print(DbHandler.get_credits(35269564))
+print(DbHandler.is_vip(35269564))
